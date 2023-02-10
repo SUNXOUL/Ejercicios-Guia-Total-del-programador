47c115b4acbc310db57a2f7760b0874bfc948e6d
Kevin Duran Bruno <123a2017kdb@gmail.com>
2023-02-10 03:24:29 -0400

    Ejercicios 8y9 Agregados

---
+++ Capitulo8y9/Capitulo 8/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+
+        string s = String.Format("{0:hh t dddd / yyyy /  M}",DateTime.Now);
+        Console.WriteLine(s);
+    }
+}
